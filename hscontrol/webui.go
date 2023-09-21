5bfd6cd434f5e5404760b2130554bec966c84987
Dolfly <dolfly@foxmail.com>
2023-09-21 12:30:34 +0000

    * update headscale-ui version

--- hscontrol/webui.go
+++ hscontrol/webui.go
@@ -8,15 +8,15 @@ import (
 	"github.com/gorilla/mux"
 )
 
-//go:embed templates/headscale-ui-0.1.2.zip
+//go:embed templates/headscale-ui-0.1.4.zip
 var zipdata []byte
 
 func web(router *mux.Router) {
-	router.PathPrefix("/web").Handler(func() http.Handler {
+	router.PathPrefix("/web/").Handler(func() http.Handler {
 		zfs, err := zipfs.New(zipdata, &zipfs.Options{Prefix: "headscale-ui"})
 		if err != nil {
 			return nil
 		}
-		return http.StripPrefix("/web", http.FileServer(zfs))
+		return http.StripPrefix("/web/", http.FileServer(zfs))
 	}())
 }
